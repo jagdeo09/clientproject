d0639aa8ea2496ffe41f355fba356cfa149e8224
Petal Jagdeo <petaljagdeo1@gmail.com>
2019-05-17 01:50:02 +0000

    still working

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var avengers_endgame = {
         title: "Avengers: Endgame",
         year: "2019",
         length: "3h 2m",
-        image: "https://m.media-amazon.com/images/M/MV5BNzg4MjM2NDQ4MV5BMl5BanBnXkFtZTgwMzk3MTgyODE@._V1_SX300.jpg",
+        image: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
         trailer: "https://www.youtube.com/watch?v=TcMBFSGVi1c"
 }
 
@@ -11,7 +11,7 @@ var the_notebook = {
         title: "The Notebook",
         year: "2004",
         length: "2h 4m",
-        image: "",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51Td3Eetr5L.jpg",
         trailer: "https://www.youtube.com/watch?v=FC6biTjEyZw&t=1s"
 }
 
@@ -19,7 +19,7 @@ var the_shining = {
         title: "The Shining",
         year: "1980",
         length: "2h 26m",
-        image: "",
+        image: "https://cdn.shopify.com/s/files/1/1057/4964/products/the-shining-vintage-movie-poster-original-1-sheet-27x41-6979.jpg?v=1535845561",
         trailer: "https://www.youtube.com/watch?v=S014oGZiSdI"
 }
 
@@ -27,7 +27,7 @@ var bridesmaids = {
         title: "Bridesmaids",
         year: "2011",
         length: "2h 12m",
-        image: "",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51YOA5yQKML.jpg",
         trailer: "https://www.youtube.com/watch?v=JgacDwgKiZg"
 }
 
@@ -37,7 +37,31 @@ function displayMovieInfo(){
 list_of_movies.forEach(function(movie){
     $("#movies").append("<p>" + movie.title + "</p>");
 });
-
+    
 }
+
+function addMovieInfo(){
+    $("#movies").add();
+    $("#images").add();
+    $("#years").add();
+    $("#lengths").add();
+    $("#trailers").add();
+}
+
+$("#add").click(function() {
+    var movieN = $("#movie").val();
+    var imageU = $("#image").val();
+    var year = $("#year").val();
+    var movieL = $("#length").val();
+    var trailer = $("#trailer").val();
+
+    list_of_movies.push(movieN);
+    list_of_movies.push(year);
+    list_of_movies.push(imageU);
+    list_of_movies.push(movieL);
+    list_of_movies.push(trailer);
+    addMovieInfo();
+    displayMovieInfo();
+});
 
 displayMovieInfo();
